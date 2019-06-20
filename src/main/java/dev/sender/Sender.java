7add7b777b2783ff8bb496eb395b7231d2f592b8
VinchoArtur <artesminitil@gmail.com>
2019-06-20 15:09:01 +0300

    Create send message

--- src/main/java/dev/sender/Sender.java
+++ src/main/java/dev/sender/Sender.java
@@ -1,5 +1,8 @@
 package dev.sender;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.util.Properties;
 
 public class Sender {
@@ -10,5 +13,30 @@ public class Sender {
     public Sender(String username, String password) {
         this.username = username;
         this.password = password;
+
+        props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+    }
+
+    public void send(String subject, String text, String fromEmail, String toEmail) {
+        Session session = Session.getInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
+            message.setSubject(subject);
+            message.setText(text);
+            Transport.send(message);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
